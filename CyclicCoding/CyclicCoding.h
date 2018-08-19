72a38b8ca9743f740e3a895135866710dc77a738
Greg Omelaenko <greg@omelaen.co>
2018-08-20 04:40:58 +1000

    Initial Commit

---
+++ CyclicCoding/CyclicCoding.h
@@ -0,0 +1,19 @@
+//
+//  CyclicCoding.h
+//  CyclicCoding
+//
+//  Created by Greg Omelaenko on 20/8/18.
+//  Copyright © 2018 Greg Omelaenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CyclicCoding.
+FOUNDATION_EXPORT double CyclicCodingVersionNumber;
+
+//! Project version string for CyclicCoding.
+FOUNDATION_EXPORT const unsigned char CyclicCodingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CyclicCoding/PublicHeader.h>
+
+
