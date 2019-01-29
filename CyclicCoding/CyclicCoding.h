7341f8c9c059582905da4737a2831fb14477449e
Greg Omelaenko <greg@omelaen.co>
2019-01-28 21:09:49 -0800

    Add Mac framework targe; share schemes

--- CyclicCoding/CyclicCoding.h
+++ CyclicCoding/CyclicCoding.h
@@ -6,7 +6,7 @@
 //  Copyright © 2018 Greg Omelaenko. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for CyclicCoding.
 FOUNDATION_EXPORT double CyclicCodingVersionNumber;
